8661bc5dc778503d93ba90e2e32310e71fe7ee01
eekelof <erik.ekelof1@gmail.com>
2025-09-07 14:07:28 +0200

    Updated deps

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     esbuild: {
         jsx: "transform",
-        jsxDev: false,
-        jsxImportSource: "vikk",
-        jsxInject: `import vikk from 'vikk'`,
-        jsxFactory: "vikk"
+        jsxFactory: "vikk",
+        jsxInject: `import vikk from 'vikk'`
     }
 });
